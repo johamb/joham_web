e9ca68bfd09f7b9b8c8bae0e9f6884caf41a6848
Jonathan Hamberger <jo.hamberger@live.de>
2019-10-30 18:02:20 +0100

    add template and icon

--- joham_web/settings.py
+++ joham_web/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '23g)gq!dcgztowxfv%27vq03+qp(au#u-c=d+%5&466(sy%+6f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['joham-web.azurewebsites.net']
+ALLOWED_HOSTS = [
+    'joham-web.azurewebsites.net',
+    'localhost',
+    ]
 
 
 # Application definition
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 ]
 
 MIDDLEWARE = [
